db4704b6499e554c414676ba725846797be2c699
Linus Sherrill <linus.sherrill@kitware.com>
2016-08-19 16:21:20 -0400

    Fix typo in boost depends include

--- CMake/kwant-depends.cmake
+++ CMake/kwant-depends.cmake
@@ -1,6 +1,5 @@
 # Central location for KWANT external dependency declaration and resolution
 
-#include( kwant-depends-vital )
 #include( kwant-depends-GeographicLib )
 include( kwant-depends-kwiver )
 include( kwant-depends-fletch )
