169249103d79e251c4609540bf77d56fe7ef9fb8
Matt Dawkins <matt.dawkins@kitware.com>
2017-02-23 05:33:36 -0500

    Update syntax to be more universal

--- CMake/kwant-depends-kwiver.cmake
+++ CMake/kwant-depends-kwiver.cmake
@@ -1,6 +1,7 @@
 find_package( kwiver REQUIRED )
-if( NOT( ${KWIVER_LIBRARIES} MATCHES track_oracle))
-  message(FATAL_ERROR "Kwant requires that kwiver be built with track_oracle enabled.")
+
+if( NOT ";${KWIVER_LIBRARIES};" MATCHES ";track_oracle;" )
+  message( FATAL_ERROR "Kwant requires that kwiver be built with track_oracle enabled." )
 endif()
 
 include_directories( SYSTEM ${KWIVER_INCLUDE_DIRS} )
