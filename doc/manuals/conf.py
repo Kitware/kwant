99de486042f9756dfa5008e175227d488e1ea635
Aaron Bray <aaron.bray@kitware.com>
2018-01-11 17:43:08 -0500

    Disable doxygen for now

--- doc/manuals/conf.py
+++ doc/manuals/conf.py
@@ -44,10 +44,11 @@ extensions = [
 breathe_projects = { "kwant": "./_build/xml" }
 breathe_default_project = "kwant"
 
-import subprocess, os
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-if read_the_docs_build:
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+# UNCOMMENT IF YOU WANT DOXYGEN TO RUN
+#import subprocess, os
+#read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+#if read_the_docs_build:
+#    subprocess.call('cd ../doxygen; doxygen', shell=True)
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
